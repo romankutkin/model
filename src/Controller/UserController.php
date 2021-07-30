9e6c680b3cf940557ccb8ce1ee55d31c3ce500b5
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2021-07-30 15:55:30 +0700

    Add deserialization request to user register command

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DataObject\UserRegisterCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
     /**
      * Registers a new user.
      */
-    public function register(): JsonResponse
-    {
+    public function register(
+        Request $request,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        $registerCommand = $serializer->deserialize(
+            $request->getContent(),
+            UserRegisterCommand::class,
+            'json'
+        );
+
         return new JsonResponse(
             status: Response::HTTP_NO_CONTENT
         );
