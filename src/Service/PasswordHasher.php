999ad567be6a65441c6bca3e7b203ee3a7cd110d
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2021-09-07 18:44:46 +0700

    Fix autowire in password hasher service

--- src/Service/PasswordHasher.php
+++ src/Service/PasswordHasher.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 
 class PasswordHasher
 {
     private PasswordHasherInterface $passwordHasher;
 
-    public function __construct(PasswordHasherFactory $factory)
+    public function __construct(PasswordHasherFactoryInterface $factory)
     {
         $this->passwordHasher = $factory->getPasswordHasher(User::class);
     }
