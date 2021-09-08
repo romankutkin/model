be4a6f276e7ecfa6b1bb927b37f64bb7f7f4ed26
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2021-09-08 17:13:34 +0700

    Create unit test for user service

---
+++ tests/Service/UserService/UserServiceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Service\UserService;
+
+use App\DataObject\UserRegisterRequest;
+use App\Service\PasswordHasher;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserServiceTest extends KernelTestCase
+{
+    public function testCreateUser()
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        /** @var UserService $userService */
+        $userService = $container->get(UserService::class);
+        $passwordHasher = $container->get(PasswordHasher::class);
+
+        $data = [
+            'firstName' => 'Roman',
+            'lastName' => 'Kut\'kin',
+            'username' => 'romankutkin',
+            'plainPassword' => 'secret',
+        ];
+
+        $userRegisterRequest = new UserRegisterRequest();
+
+        $userRegisterRequest
+            ->setFirstName($data['firstName'])
+            ->setLastName($data['lastName'])
+            ->setUsername($data['username'])
+            ->setPlainPassword($data['plainPassword'])
+        ;
+
+        $user = $userService->create($userRegisterRequest);
+
+        $this->assertEquals($user->getFirstName(), $data['firstName']);
+        $this->assertEquals($user->getLastName(), $data['lastName']);
+        $this->assertEquals($user->getUsername(), $data['username']);
+        $this->assertEquals($user->getPassword(), $passwordHasher->hash($data['plainPassword']));
+    }
+}
